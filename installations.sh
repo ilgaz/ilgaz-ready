f64bee71d5723b481e3191931dd5d5508a522d88
Ilgaz Can <ilgzcnn@gmail.com>
2020-02-16 19:45:02 +0300

    :wrench: Add a fingerprint check for Docker

--- installations.sh
+++ installations.sh
@@ -10,9 +10,11 @@ sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
 sudo apt-get update -y
 
-sudo apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
